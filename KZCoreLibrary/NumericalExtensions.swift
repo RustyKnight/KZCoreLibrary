42ad9c42d299b3e9272e4fa9b75988f3b277400b
Shane Whitehead <obishiro@gmail.com>
2016-03-26 09:40:23 +1100

    Updating non-public properties to public

--- KZCoreLibrary/NumericalExtensions.swift
+++ KZCoreLibrary/NumericalExtensions.swift
@@ -11,27 +11,27 @@ import Foundation
 //----- Core numerical extensions -----//
 
 public extension Float {
-	var toRadians : Float {
+	public var toRadians : Float {
 		return self * Float(M_PI) / 180.0
 	}
-	var toDegrees: Float {
+	public var toDegrees: Float {
 		return self * 180 / Float(M_PI)
 	}
-	var toDouble: Double {
+	public var toDouble: Double {
 		return Double(self)
 	}
 }
 
 public extension Double {
-	var toRadians : Double {
+	public var toRadians : Double {
 		return self * M_PI / 180.0
 	}
 	
-	var toDegrees: Double {
+	public var toDegrees: Double {
 		return self * 180 / M_PI
 	}
 	
-	var toFloat: Float {
+	public var toFloat: Float {
 		return Float(self)
 	}
 }
